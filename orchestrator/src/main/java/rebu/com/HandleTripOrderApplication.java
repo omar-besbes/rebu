ff678fbfe82756540865e1d68a0cd68064013f8b
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2025-01-21 08:10:13 +0100

    feat: orchestrator service implemented using BPMN 2.0 engine camunda

---
+++ orchestrator/src/main/java/rebu/com/HandleTripOrderApplication.java
@@ -0,0 +1,9 @@
+package rebu.com;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("HandleTripOrder App")
+public class HandleTripOrderApplication extends ServletProcessApplication {
+    // empty implementation
+}
